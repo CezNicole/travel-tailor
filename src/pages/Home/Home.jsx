7be373ea3cff73e853c475ccd409c9245554ba40
Cez Nicole Comia <cezcomia@gmail.com>
2024-06-07 12:03:59 -0400

    fix bug with province/territory dropdown and add logic to capitalize first letter of each province word

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
     const [endDate, setEndDate] = useState(new Date());
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
+    const [showAttractions, setShowAttractions] = useState(false);
 
     const getSeason = (date) => {
         const month = date.getMonth() + 1;
@@ -42,7 +42,6 @@ export default function Home() {
             return 'all time';
         }
     }
-
     
     useEffect(() => {
         const fetchProvinces = async () => {
@@ -59,54 +58,32 @@ export default function Home() {
 
     }, [serverUrl]);
     
-    // const handleProvinceChange = (event) => {
-    //     console.log(event.target.value);
-    //     setSelectedProvince(event.target.value);
-    // }
-
     const fetchAttractions = async (province, season) => {
         setLoading(true);
-
+        
         try {
-            const response = await axios.get(`${serverUrl}/api/attractions/${province}/${season}`);
+            const season = getSeason(startDate);
+            const response = await axios.get(`${serverUrl}/api/attractions/${selectedProvince}/${season}`);
             console.log("Attractions: ", response.data);
             setAttractions(response.data);
+            setShowAttractions(true);
             setError(null);
         } catch (error) {
             console.error("Error fetching Canadian attractions", error);
             setError(error.message);
+        } finally {
             setLoading(false);
         }
     }
 
-    useEffect(() => {
-        if(selectedProvince){
-            const season = getSeason(startDate);
-            fetchAttractions(selectedProvince, season);
-        } else{
-            setAttractions([]);
-        }
-    }, [selectedProvince, startDate, serverUrl]);
-
     // useEffect(() => {
-    //     const fetchAttractions = async () => {
-    //         try {
-    //             if(selectedProvince){
-    //                 const response = await axios.get(`${serverUrl}/api/attractions/${selectedProvince}`);
-    //                 console.log("Attractions: ", response.data);
-    //             } else{
-    //                 console.log(setAttractions([]));
-    //                 setAttractions([]);
-    //             }
-    //         } catch (error) {
-    //             console.error("Error fetching Canadian attractions", error);
-    //         }
+    //     if(selectedProvince){
+    //         const season = getSeason(startDate);
+    //         fetchAttractions(selectedProvince, season);
+    //     } else{
+    //         setAttractions([]);
     //     }
-
-    //     fetchAttractions();
-
-    // }, [selectedProvince, serverUrl])
-
+    // }, [selectedProvince, startDate, serverUrl]);
 
     const handleProvinceChange = (event) => {
         console.log(event.target.value);
@@ -120,32 +97,18 @@ export default function Home() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
-        // const fetchAttractions = async () => {
-        //     try {
-        //         const response = await axios.get(`${serverUrl}/api/attractions/${selectedProvince}`);
-        //         console.log(response.data);
-        //         setAttractions(response.data);
-        //     } catch (error) {
-        //         console.error("Error fetching Canadian attractions", error);   
-        //     }
+        // const season = getSeason(startDate);
+        // if(selectedProvince && season){
+        //     fetchAttractions(selectedProvince, season)
+        // } else{
+        //     console.log("Selected province/territory or season is undefined");
         // }
-
-        const season = getSeason(startDate);
-        if(selectedProvince && season){
-            fetchAttractions(selectedProvince, season)
-        } else{
-            console.log("Selected province/territory or season is undefined");
-        }
 
         fetchAttractions();
     }
 
     return(
         <>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
-            
             <h1>Travel Tailor</h1>
             <h2>Your next Canadian adventure awaits...</h2>
             <form onSubmit={handleSubmit}>
@@ -156,40 +119,62 @@ export default function Home() {
                 >
                     {/* <option key={0} value={0}>Select a Province/Territory</option> */}
                     <option value="">Select a Province/Territory</option>
-                    {provinces.map((province) => {
+                    {provinces.map((province, index) => {
+                        const words = province.split(" ");
+                        const capitalizedProvince = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
                         console.log(province);
                         return(
-                            <option key={province} value={province}>
-                                {province.province_territory}
+                            <option key={index} value={province}>
+                                {capitalizedProvince}
                             </option>
                         )
                     })}
                 </select>
                 <p>Selected Province/Territory: {selectedProvince}</p>
 
-                <ul>
-                    {attractions.map((attraction) => (
-                        <li key={attraction.id}>
-                            {attraction.attraction_name}
-                            Attraction Type: {attraction.attraction_type}
-                            Best Time to Visit: {attraction.best_time_to_visit}
-                            Visiting Hours: {attraction.visiting_hours}
-                            Address: {attraction.address}
-                            Website: <a href={attraction.website_link}>{attraction.website_link}</a>
-                            <img src={attraction.image_link} alt={attraction.attraction_name} />
-                        </li>
-
-                    ))}
-                </ul>
-
-                
                 <CalendarSelector onDateChange={handleDateChange} />
 
                 <button className="form__button">
-                    Let's Travel, Eh? 
-                    <img src={airplaneIcon} alt="airplane icon" className="form__button-icon"/>
+                    Let's Travel, Eh?
+                    <img src={airplaneIcon} alt="airplane icon" className="form__button-icon" />
                 </button>
             </form>
+
+
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+
+
+            {/* {!showAttractions && attractions.length === 0 && 
+                <p>Sorry, no attractions available for the selected province/territory and/or season. 
+                    Please modify your selection.
+                </p>
+            } */}
+
+            {!loading && showAttractions && attractions.length === 0 &&
+                <p>
+                    Sorry, no attractions available for the selected province/territory and/or season.
+                    Please modify your selection.
+                </p>
+            }
+
+            {!loading && showAttractions && attractions.length > 0 && (
+                <>
+                    <ul>
+                        {attractions.map((attraction) => (
+                            <li key={attraction.id}>
+                                {attraction.attraction_name}<br></br>
+                                Attraction Type: {attraction.attraction_type}<br></br>
+                                Best Time to Visit: {attraction.best_time_to_visit}<br></br>
+                                Visiting Hours: {attraction.visiting_hours}<br></br>
+                                Address: {attraction.address}<br></br>
+                                Website: <a href={attraction.website_link}>{attraction.website_link}</a><br></br>
+                                <img src={attraction.image_link} alt={attraction.attraction_name} /><br></br>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
         </>
     )
 }

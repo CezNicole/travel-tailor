9b9fffe030a1989a57006eaf1a9775dcf6fbfa94
CezNicole <cezcomia@gmail.com>
2025-03-13 17:25:56 -0400

    Fix typo error on footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 export default function Footer(){
     return(
         <footer className="footer">
-            <p className="footer__content">Copyrignt &copy; 2024. All rights reserved</p>
+            <p className="footer__content">Copyright &copy; 2024. All rights reserved</p>
         </footer>
     )
 }
